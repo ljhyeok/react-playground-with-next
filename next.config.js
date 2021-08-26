720de0e18f8cdfdc7eacbdf9a880cede8d278e7c
front_임준혁 <jhlym92@gmail.com>
2021-08-26 17:40:07 +0900

    feat: webpack svgr 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  rules: [
+    {
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ['@svgr/webpack'],
+    },
+  ],
+};
